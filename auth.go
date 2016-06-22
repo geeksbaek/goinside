977d91069bc07a42ac70a6c397fa47fcde2ea694
백종열 <geeksbaek@gmail.com>
2016-06-23 02:47:52 +0900

    add comment

--- auth.go
+++ auth.go
@@ -14,15 +14,17 @@ type Auth struct {
 	transport *http.Transport
 }
 
-func LoginNomember(id, pw string) *Auth {
+// GetNomemberAuth 함수는 전달받은 ID, PASSWORD로 생성한 비회원 객체를 반환합니다.
+func GetNomemberAuth(id, pw string) *Auth {
 	return &Auth{id: id, pw: pw, nomember: true}
 }
 
-func Login(id, pw string) (*Auth, error) {
-	// return &Auth{id: id, pw: pw, nomember: false}
-	return nil, nil
-}
+// func Login(id, pw string) (*Auth, error) {
+// 	// return &Auth{id: id, pw: pw, nomember: false}
+// 	return nil, nil
+// }
 
+// SetTransport 함수는 해당 객체가 주어진 프록시를 통해 디시인사이드와 통신하도록 설정합니다.
 func (a *Auth) SetTransport(URL *url.URL) {
 	a.transport = &http.Transport{Proxy: http.ProxyURL(URL)}
 }
