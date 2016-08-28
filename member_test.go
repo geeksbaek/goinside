61453a129643ac4fb6e9755430631be50715147c
백종열 <geeksbaek@gmail.com>
2016-08-28 20:59:27 +0900

    rm login test

--- member_test.go
+++ member_test.go
@@ -1,26 +1,26 @@
 package goinside
 
-import (
-	"encoding/json"
-	"io/ioutil"
-	"testing"
-)
+// import (
+// 	"encoding/json"
+// 	"io/ioutil"
+// 	"testing"
+// )
 
-func TestLogin(t *testing.T) {
-	authFile, err := ioutil.ReadFile("auth.json")
-	if err != nil {
-		t.Error(err)
-	}
-	var auth struct {
-		ID string
-		PW string
-	}
-	err = json.Unmarshal(authFile, &auth)
-	if err != nil {
-		t.Error(err)
-	}
-	_, err = Login(auth.ID, auth.PW)
-	if err != nil {
-		t.Error(err)
-	}
-}
+// func TestLogin(t *testing.T) {
+// 	authFile, err := ioutil.ReadFile("auth.json")
+// 	if err != nil {
+// 		t.Error(err)
+// 	}
+// 	var auth struct {
+// 		ID string
+// 		PW string
+// 	}
+// 	err = json.Unmarshal(authFile, &auth)
+// 	if err != nil {
+// 		t.Error(err)
+// 	}
+// 	_, err = Login(auth.ID, auth.PW)
+// 	if err != nil {
+// 		t.Error(err)
+// 	}
+// }
