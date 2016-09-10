4d6983a7768b535e2208ec1fe88ba3c1c9c71582
백종열 <geeksbaek@gmail.com>
2016-09-11 01:28:15 +0900

    init

---
+++ search.go
@@ -0,0 +1,23 @@
+package goinside
+
+/*
+
+제목+내용
+http://m.dcinside.com/api/gall_list.php?app_id=blM1T09mWjRhQXlZbE1ML21xbkM3QT09&s_type=subject_m&id=programming&page=1&serVal=%ED%81%B4%EB%A6%AC%EB%84%88
+
+전체
+http://m.dcinside.com/api/gall_list.php?app_id=blM1T09mWjRhQXlZbE1ML21xbkM3QT09&s_type=all&id=programming&page=1&serVal=%ED%81%B4%EB%A6%AC%EB%84%88
+
+제목
+http://m.dcinside.com/api/gall_list.php?app_id=blM1T09mWjRhQXlZbE1ML21xbkM3QT09&s_type=subject&id=programming&page=1&serVal=%ED%81%B4%EB%A6%AC%EB%84%88
+
+내용
+http://m.dcinside.com/api/gall_list.php?app_id=blM1T09mWjRhQXlZbE1ML21xbkM3QT09&s_type=memo&id=programming&page=1&serVal=%ED%81%B4%EB%A6%AC%EB%84%88
+
+글쓴이
+http://m.dcinside.com/api/gall_list.php?app_id=blM1T09mWjRhQXlZbE1ML21xbkM3QT09&s_type=name&id=programming&page=1&serVal=%ED%81%B4%EB%A6%AC%EB%84%88
+
+http://m.dcinside.com/api/gall_list.php?app_id=blM1T09mWjRhQXlZbE1ML21xbkM3QT09&s_type=name&id=programming&page=1&ser_pos=-586528&serVal=%ED%81%B4%EB%A6%AC%EB%84%887
+http://m.dcinside.com/api/gall_list.php?app_id=blM1T09mWjRhQXlZbE1ML21xbkM3QT09&s_type=name&id=programming&page=1&ser_pos=-576528&serVal=%ED%81%B4%EB%A6%AC%EB%84%887
+
+*/
