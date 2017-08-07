a68c96d2404b2fe2af11d3ea81ec4b896530cff4
Jongyeol Baek <geeksbaek@gmail.com>
2017-08-08 06:08:16 +0900

    Update doc.go

--- gallog/doc.go
+++ gallog/doc.go
@@ -5,6 +5,7 @@ Package gallog 는 디시인사이드 갤로그 관련 API를 제공합니다.
 Web을 파싱하는 방법으로 API를 구현하였습니다.
 
 gallog 패키지로 구현한 디시인사이드 클리너는 아래 링크에서 확인할 수 있습니다.
+
 https://github.com/geeksbaek/goinside-gallog-cleaner
 */
 package gallog
